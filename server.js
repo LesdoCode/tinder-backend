d8c2c34772da4cced13a83db19bcf846bf96dfe1
Tebogo Lemekoana <lesdocode@gmail.com>
2021-07-13 22:20:42 +0200

    complete and test post and get requests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cards from './dbCards.js';
+import Cors from 'cors';
 
 //App config
 const app = express();
@@ -8,6 +10,8 @@ const connection_url = 'mongodb+srv://admin:WfeqT2gXnBFyR33@cluster0.9wihv.mongo
 
 
 //Middleware
+app.use(express.json());
+app.use(Cors())
 
 
 //DB config
@@ -17,10 +21,35 @@ mongoose.connect(connection_url, {
     useUnifiedTopology: true,
 }) //This is used because MongoDB is under constant evolution/ evolvation
 
+
 //API endpoints
 app.get('/', (req, res) => {
     res.status(200).send("Thank You Jesus");
-})
+});
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+    Cards.create(dbCard, (err, data) => {
+        if (err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
 
 //Listener
 app.listen(port, () => {
